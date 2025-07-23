8afa52ed21c480f1fb2503d5a3d6e21994103c88
helozinha1 <helowzx@gmail.com>
2025-07-23 17:37:33 -0300

    Versão 0.0.8

--- netlify/functions/get-news.js
+++ netlify/functions/get-news.js
@@ -15,7 +15,7 @@ exports.handler = async function(event, context) {
         };
     }
 
-    const query = 'desastre%20ambiental'; // Mudei de 'meio ambiente' para 'desastre ambiental' para refletir o novo foco
+    const query = 'desastre ambiental, meio ambiente, mudança climatica'; // Mudei de 'meio ambiente' para 'desastre ambiental' para refletir o novo foco
     const language = 'pt';
     const sortBy = 'relevancy';
     // REMOVIDO: const fromDate = '2025-07-23'; 
